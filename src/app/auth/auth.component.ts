3a9a069802d06cb42f18bb045c7f99710082eb5a
salshaikhi <salshaikhi@tahakom.com>
2022-11-10 11:25:16 +0300

    authentication form done

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector:'app-auth',
@@ -6,5 +7,13 @@ import { Component } from "@angular/core";
     styleUrls: ['auth.component.scss']
 })
 export class AuthComponent{
+    isLoginMode = true;
 
+    onSwitchMode(){
+        this.isLoginMode = !this.isLoginMode;
+    }
+    onSubmit(authForm:NgForm){
+        console.log(authForm.value);
+        authForm.reset();
+    }
 }

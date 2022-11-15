ec3f6cd0d11210bdb4c8a1da7a7420acccc060e3
salshaikhi <salshaikhi@tahakom.com>
2022-11-15 15:49:47 +0300

    implementing rxjs store

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Subscription, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -9,18 +11,25 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients:Ingredient[];
+  ingredientsValue:{ ingredients: Ingredient[] };
   private igChangeSub:Subscription;
-  constructor(private shoppingListService:ShoppingListService) { }
+  constructor(
+    private shoppingListService:ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    ) { }
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
+     this.store.select('shoppingList').pipe(take(1)).subscribe(ingredients => {
+      this.ingredientsValue = ingredients;
+    });
+    // this.ingredients = this.shoppingListService.getIngredients();   
+    // this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
+    //   (reIngredients:Ingredient[])=>{
+    //     this.ingredients = reIngredients;
+    //   }
+    // );
+    console.log('store selected',this.store.select('shoppingList'));
     
-    this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
-      (reIngredients:Ingredient[])=>{
-        this.ingredients = reIngredients;
-      }
-    );
   }
   onEditItem(index:number){
     this.shoppingListService.startedEditing.next(index);
